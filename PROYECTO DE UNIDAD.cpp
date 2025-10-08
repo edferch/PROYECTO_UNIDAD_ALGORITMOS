d43751d9f08e3eb9d9861849346e6412bb41540f
Ferch <chaconfernando3110@gmail.com>
2025-10-08 14:22:57 -0600

    Cree las estructuras

--- PROYECTO DE UNIDAD.cpp
+++ PROYECTO DE UNIDAD.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
+#include<string>
+#include<fstream>
 
 using namespace std;
+
+struct Libro {
+	int id;
+	string titulo;
+	string autor;
+	string año;
+	string categoria;
+};
+
+struct Usuario {
+	int id;
+	string nombre;
+	string correo;
+};
+
+struct Libro {
+	int id;
+	int idUsuario;
+	int idLibro;
+	string FechaPrestamo;
+	string FechaDevolucion;
+};
 
 int main() {
 
